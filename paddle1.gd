498dd94c5de3d4ba9de05dd3e42c773009994dac
Serjoe87 <desocron@gmail.com>
2025-11-12 14:10:33 +0100

    0.3
    Paddles can be controlled now.

---
+++ paddle1.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var speed = 500
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("Player1_up"):
+		velocity.y = -1
+	elif Input.is_action_pressed("Player1_down"):
+		velocity.y = 1
+	else:
+		velocity.y = 0
+	
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
